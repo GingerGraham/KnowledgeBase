c0cb77fdaa9edbfc845323aaaff879333f9ee703
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2022-02-19 18:10:38 +0000

    Debugging - echoing pwd

--- .github/scripts/addFrontMatter.sh
+++ .github/scripts/addFrontMatter.sh
@@ -5,6 +5,8 @@ startEnd="---"
 collection="collection: knowledge"
 tab="    "
 
+echo ${pwd}
+
 for path in $(find ${basePath} -type f \( -iname "*.md" ! -iname "*index*" ! -iname "*readme*" \) -print); do
   category=$(dirname ${path} | awk -F "/" '{print $NF}') # generate a category from the parent directory of the file
   frontMatter="${startEnd}\n${collection}\ncategories:\n${tab}- ${category}\n${startEnd}" # build Liquid Front Matter - builds multiline string
