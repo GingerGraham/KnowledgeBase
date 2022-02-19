ead4eb89f3fe053372a734bc1eb160911017aaf4
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2022-02-19 21:04:47 +0000

    Correcting issues with front matter formatting

--- .github/scripts/addFrontMatter.sh
+++ .github/scripts/addFrontMatter.sh
@@ -4,12 +4,11 @@ basePath="./KnowledgeBase/"
 startEnd="---"
 tab="    "
 
-echo ${pwd}
-
 for path in $(find ${basePath} -type f \( -iname "*.md" ! -iname "*index*" ! -iname "*readme*" \) -print); do
-  subject=$(dirname ${path} | awk -F "/" '{print tolower $NF}') # generate a category from the parent directory of the file - in lowercase
-  title=$(basename -s .md ${path} | awk '{print tolower $0}') # generate a permalink from the file name - in lowercase
-  titleSpaces=${title//-/ } # replace hyphens with spaces
-  frontMatter="${startEnd}\npermalink: /${subject}/${title}/\nsubject: ${subject}\ntitle: ${titleSpaces}\n${startEnd}" # build Liquid Front Matter - builds multiline string
+  subject=$(dirname ${path} | awk -F "/" '{print tolower($NF)}') # generate a category from the parent directory of the file - in lowercase
+  title=$(basename -s .md ${path}) # generate a title from the file name
+  titleLower=$(echo ${title} | awk '{print tolower($0)}') # set the title to lowercase
+  titleSpaces=$(echo ${title} | tr "-" " " ) # replace hyphens with spaces
+  frontMatter="${startEnd}\npermalink: /${subject}/${titleLower}/\nsubject: ${subject}\ntitle: ${titleSpaces}\n${startEnd}" # build Liquid Front Matter - builds multiline string
   sed -i "1i ${frontMatter}" ${path} # prepend the front matter to the head of the file
 done
