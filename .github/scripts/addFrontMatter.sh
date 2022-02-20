e3b918bab979fd8ef56161ac343ed1dcda81b8b5
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2022-02-20 16:12:32 +0000

    Adjustment to permalink path

--- .github/scripts/addFrontMatter.sh
+++ .github/scripts/addFrontMatter.sh
@@ -11,6 +11,6 @@ for path in $(find ${basePath} -type f \( -iname "*.md" ! -iname "*index*" ! -in
   titleLower=$(echo ${title} | awk '{print tolower($0)}') # set the title to lowercase
   titleSpaces=$(echo ${title} | tr "-" " " ) # replace hyphens with spaces
   excerpt=$(sed -n 3p ${path}) # get the first non-title line of the file
-  frontMatter="${startEnd}\npermalink: /${subjectLower}/${titleLower}/\nsubject: ${subject}\ntitle: ${titleSpaces}\nexcerpt: "${excerpt}"\n${startEnd}" # build Liquid Front Matter - builds multiline string
+  frontMatter="${startEnd}\npermalink: /knowledge/${subjectLower}/${titleLower}/\nsubject: ${subject}\ntitle: ${titleSpaces}\nexcerpt: "${excerpt}"\n${startEnd}" # build Liquid Front Matter - builds multiline string
   sed -i "1i ${frontMatter}" ${path} # prepend the front matter to the head of the file
 done
