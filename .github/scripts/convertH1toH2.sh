4a40c2cda178592ed6b7cc0ac43a57fe649adec2
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2024-04-03 08:01:37 +0100

    Reverting changes to path loop

--- .github/scripts/convertH1toH2.sh
+++ .github/scripts/convertH1toH2.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 basePath="./KnowledgeBase/"
 
 echo "[DEBUG] Converting all H1 to H2 in all markdown files in ${basePath}..."
 
-find ${basePath} -type f \( -iname "*.md" ! -iname "*index*" ! -iname "*readme*" \) -exec sh -c '
-  path="$1"
-  echo "[DEBUG] Converting all H1 to H2 in ${path}..."
-  # Replace all instances of # with ## - this converts all H1 to H2
-  sed -i 's/\(^#\)\(\s\)/\#\#\2/g' "${path}"
-  echo "[DEBUG] All H1 converted to H2 in ${path}."
-' sh {} \;
+for path in $(find ${basePath} -type f \( -iname "*.md" ! -iname "*index*" ! -iname "*readme*" \) -print); do
   echo "[DEBUG] Converting all H1 to H2 in ${path}..."
   # Replace all instances of # with ## - this converts all H1 to H2
   sed -i 's/\(^#\)\(\s\)/\#\#\2/g' "${path}"
